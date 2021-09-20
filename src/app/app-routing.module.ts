44342b50744ceb159ae29ebd1dc9378320c4efee
Taiwo  Gbemisola Bello <tbello@ecobank.com>
2021-09-20 11:13:17 +0100

    Login researchers page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './Guard/auth';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+import { StatisticsComponent } from './statistics/statistics.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  { path: 'statistics', component: StatisticsComponent, canActivate: [AuthGuard] },
+  { path: '', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
